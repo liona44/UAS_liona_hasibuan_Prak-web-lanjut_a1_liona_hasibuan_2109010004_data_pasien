ce615294481d36550cfa55fa0d2f91eae05b76b0
liona44 <hasibuanliona635@gmail.com>
2024-06-23 02:02:45 +0700

    uas web lamjut

---
+++ resources/views/layouts/include/_navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+
+    <div class="container-fluid" style="background-color: rgb(218, 87, 196);">
+        <div class="navbar-btn navbar-btn-left" style="padding-left: 40px; background-color: rgb(218, 87, 196); ">
+            <a class="btn btn-secondary update-pro" style="background-color: rgb(218, 87, 196);" href="#"
+                title="Upgrade to Pro">
+                <span style="color: white">Rumah Sakit Liona</span>
+            </a>
+        </div>
+
+        <div id="navbar-menu" style="padding-right: 50px">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                        style="background-color: rgb(218, 87, 196);">
+
+                        <span style="color:white;background-color: rgb(218, 87, 196);">Menu</span> <i
+                            class="icon-submenu lnr lnr-chevron-down"
+                            style="color:white;background-color: rgb(218, 87, 196);"></i></a>
+                    <ul class="dropdown-menu" style="background-color: rgb(218, 87, 196);">
+                        <li><a href="/pasien" style="color: white; background-color: rgb(218, 87, 196);">data pasien</a>
+                        </li>
+                        <li><a href="/" onclick="return confirm('Yakin mau Keluar?')"
+                                style="color: white; background-color: rgb(218, 87, 196);">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
